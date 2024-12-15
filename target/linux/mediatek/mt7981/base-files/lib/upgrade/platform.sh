81884c80b73a0eba9cfeb430e8f5f22ce2995ae6
thunder361 <73006038+thunder361@users.noreply.github.com>
2024-12-16 00:46:15 +0800

    Update platform.sh

--- target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
@@ -191,6 +191,7 @@ platform_do_upgrade() {
 	xiaomi,mi-router-wr30u-112m|\
 	xiaomi,mi-router-ax3000t|\
 	xiaomi,mi-router-ax3000t-an8855|\
+ 	one,r3-mini |\
 	*mt3000* |\
 	glinet,x3000-emmc |\
 	*xe3000* |\
@@ -244,6 +245,7 @@ platform_check_image() {
 	[ "$#" -gt 1 ] && return 1
 
 	case "$board" in
+ 	one,r3-mini |\
 	*mt3000* |\
 	glinet,x3000-emmc |\
 	*xe3000* |\
